ab7f8bfd635de2e4e7fb06183e8484bc9af52ef0
Nahuel Bentos <nahuel.bentos@fing.edu.uy>
2021-07-23 10:59:39 -0300

    Avance seccion 9

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,56 @@
-import {interval, from, fromEvent} from 'rxjs';
-import {auditTime, sample, tap, map} from 'rxjs/operators';
+import { fromEvent, Observable } from 'rxjs';
+import { debounceTime, map, pluck, mergeAll } from 'rxjs/operators';
 
-const click$ = fromEvent<MouseEvent>(document, 'click');
+import { ajax } from 'rxjs/ajax';
 
-click$
-  .pipe(
-    map(({x}) => ({x})),
-    tap((val) => console.log('tap ', val)),
-    auditTime(2000)
-  )
-  .subscribe(console.log);
+import { GithubUser } from './interfaces/github-user.interface';
+import { GithubUsersResp } from './interfaces/github-users.interface';
+
+
+// Referencias
+const body = document.querySelector('body');
+const textInput = document.createElement('input');
+const orderList = document.createElement('ol');
+body.append( textInput, orderList );
+
+// Helpers
+const mostrarUsuarios = ( usuarios: GithubUser[] ) => {
+    
+    console.log(usuarios);
+    orderList.innerHTML = '';
+
+    for( const usuario of usuarios ) {
+
+        const li  = document.createElement('li');
+        const img = document.createElement('img');
+        img.src = usuario.avatar_url;
+
+        const anchor  = document.createElement('a');
+        anchor.href   = usuario.html_url;
+        anchor.text   = 'Ver página';
+        anchor.target = '_blank';
+
+        li.append( img );
+        li.append( usuario.login + ' ' );
+        li.append( anchor );
+
+        orderList.append(li);
+    }
+
+}
+
+
+
+// Streams
+const input$ = fromEvent<KeyboardEvent>( textInput, 'keyup' );
+
+
+input$.pipe(
+    debounceTime<KeyboardEvent>(500),
+    pluck<KeyboardEvent, string>('target','value'),
+    map<string, Observable<GithubUsersResp>>( texto => ajax.getJSON(
+        `https://api.github.com/search/users?q=${ texto }`
+    )),
+    mergeAll<GithubUsersResp>(),
+    pluck<GithubUsersResp, GithubUser[]>('items')
+).subscribe( mostrarUsuarios );
